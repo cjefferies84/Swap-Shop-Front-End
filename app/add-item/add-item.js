af51be01f5a241c05a81172e8cfdb71fe8ef7663
Davidslc <djjohnson@gmail.com>
2015-02-10 11:24:11 -0700

    added picture upload capability to frontend

--- app/add-item/add-item.js
+++ app/add-item/add-item.js
@@ -11,10 +11,10 @@ angular.module('myApp.addItem', ['ngRoute'])
 
     .controller('AddItemCtrl', ['$scope', 'Restangular', '$location', '$http', function ($scope, Restangular, $location, $http) {
         // Takes input from the form. Currently 'user' and 'status' are hard coded
-        $scope.item = {  user: 1, status: "Available"};
+        $scope.item = {user: 1, status: "Available"};
 
         // Save fields to the item object
-        $scope.addItem = function() {
+        $scope.addItem = function () {
 
             var fd = new FormData();
             fd.append("picture", $scope.item.picture);
@@ -24,33 +24,17 @@ angular.module('myApp.addItem', ['ngRoute'])
             fd.append("status", $scope.item.status);
             fd.append("user", $scope.item.user);
 
-
-           //Restangular.all('items/').customPOST(fd).then(function () {
-           //         alert("Your swap item was successfully created");
-           //         $location.path('/home');
-           //     },
-           //     function () {
-           //         alert("There was a problem creating your swap item. Please try again.")
-           //     })
-
             $http.post('http://localhost:8001/items/', fd, {
-//            $http.post('http://vast-journey-8108.herokuapp.com/location', fd, {
-//                withCredentials: true,
-                headers: {'Content-Type': undefined },
-                transformRequest: angular.identity
+                headers: {'Content-Type': undefined}
             }).success(function (response) {
-                    $window.location = 'index.html#/home';
-                }).error(function (response) {
-                    console.log('Response: ' + response);
-                });
+                $location.path('/swap-gallery');
+            }).error(function (response) {
+                console.log('Error response: ' + response);
+            });
         };
-
-
 
         $scope.uploadFile = function (files) {
             $scope.item.picture = files[0];
-            console.log($scope.item.picture);
         };
-
 
     }]);
