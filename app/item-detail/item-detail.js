da8a2d70558da85e4b729d4bec9e7a4ed7d9b5fe
Davidslc <djjohnson@gmail.com>
2015-02-10 16:04:18 -0700

    Fixed div error and added function that shows all items on the detail-items page when a swap button is pushed on that page

--- app/item-detail/item-detail.js
+++ app/item-detail/item-detail.js
@@ -2,32 +2,46 @@
 
 angular.module('myApp.itemDetail', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/items/:itemId', {
-    templateUrl: 'item-detail/item-detail.html',
-    controller: 'ItemDetailCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/items/:itemId', {
+            templateUrl: 'item-detail/item-detail.html',
+            controller: 'ItemDetailCtrl'
+        });
+    }])
 
-.controller('ItemDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', function($scope, Restangular, $routeParams, $location) {
+    .controller('ItemDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', function ($scope, Restangular, $routeParams, $location) {
+        $scope.itemId = $routeParams.itemId;
 
-    $scope.itemId = $routeParams.itemId;
+        Restangular.one('items', $scope.itemId).customGET().then(function (data) {
+            $scope.item = data;
+        });
 
-    Restangular.one('items', $scope.itemId).customGET().then(function(data){
-        $scope.item = data;
-    });
+        Restangular.all('items/').getList().then(function (data) {
+            $scope.userItems = data;
+        });
 
-        $scope.deleteItem = function() {
-        var confirmation = confirm('Are you sure you want to delete this item? This cannot be undone');
+        $scope.deleteItem = function () {
+            var confirmation = confirm('Are you sure you want to delete this item? This cannot be undone');
 
-        if (confirmation) {
-            Restangular.one('items', $scope.itemId).customDELETE().then(function() {
-                alert('Your item was successfully deleted!');
-                $location.path('/items');
-            },
-            function() {
-                alert('There was a problem deleting your item')
-            })
+            if (confirmation) {
+                Restangular.one('items', $scope.itemId).customDELETE().then(function () {
+                        alert('Your item was successfully deleted!');
+                        $location.path('/items');
+                    },
+                    function () {
+                        alert('There was a problem deleting your item')
+                    })
+            }
+        };
+
+        $scope.swap = function() {
+            if ($scope.showItems == true) {
+                $scope.showItems = false;
+                $scope.swapButtonText = "Swap";
+            } else {
+                $scope.showItems = true;
+                $scope.swapButtonText = "Undo Swap";
+            }
+
         }
-    }
-}]);
+    }]);
