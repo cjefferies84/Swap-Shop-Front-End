0efa5b60e46cdc6dd5d8cbed74a4a85bf27eed1f
Chris Jefferies <cjefferies8439@gmail.com>
2015-02-21 12:33:41 -0700

    changed background

--- app/my-swaps/my-swaps.js
+++ app/my-swaps/my-swaps.js
@@ -15,6 +15,43 @@ angular.module('myApp.mySwaps', ['ngRoute'])
             $scope.swaps = data;
         });
 
+        $scope.initiateSwap = function (id) {
+            var confirmation = confirm('Are you sure you want to initiate this swap?');
+
+            if (confirmation) {
+                var whatIActuallyWant = null;
+                for(var ugh = 0; ugh < $scope.swaps.length; ugh++) {
+                    console.log("checking " + $scope.swaps[ugh].id);
+                    if($scope.swaps[ugh].id == id) {
+                        console.log("found it");
+                        whatIActuallyWant = $scope.swaps[ugh];
+                    }
+                }
+                console.log(whatIActuallyWant);
+                Restangular.one('swaps', id).customPUT(whatIActuallyWant).then(function () {
+                        alert('You have initiated a swap successfully!');
+                        $location.path('/swaps');
+                    },
+                    function () {
+                        alert('There was a problem initiating your item')
+                    })
+            }liz
+        };
+
+        $scope.declineSwap = function () {
+            var confirmation = confirm('Are you sure you want to decline this swap?');
+
+            if (confirmation) {
+                Restangular.one('items', $scope.swapId).customPUT().then(function () {
+                        alert('Your have declined swap successfully!');
+                        $location.path('/items');
+                    },
+                    function () {
+                        alert('There was a problem declining your swap')
+                    })
+            }
+        };
+
 
         $scope.deleteSwap = function () {
             var confirmation = confirm('Are you sure you want to delete this swap? This cannot be undone');
